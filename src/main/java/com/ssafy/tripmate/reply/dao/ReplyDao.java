530a8e31c846ddcf2dd3490dbfdfda4c0ceb37fb
yeondori <yeonseo.jung0@gmail.com>
2024-05-21 12:04:26 +0900

    feat: ReplyDao, Mapper

---
+++ src/main/java/com/ssafy/tripmate/reply/dao/ReplyDao.java
@@ -0,0 +1,21 @@
+package com.ssafy.tripmate.reply.dao;
+
+import com.ssafy.tripmate.reply.dto.Reply;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Mapper
+public interface ReplyDao {
+
+    void insert(Reply reply) throws SQLException;
+
+    List<Reply> searchAll(int boardId) throws SQLException;
+
+    Reply search(int boardId, int replyId) throws SQLException;
+
+    void update(Reply reply) throws SQLException;
+
+    void remove(int replyId) throws SQLException;
+}

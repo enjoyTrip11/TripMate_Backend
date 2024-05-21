a95cdda13c68b9bd5d3ce58b5cde4af98367b9a1
yeondori <yeonseo.jung0@gmail.com>
2024-05-21 15:48:56 +0900

    fix: PlaceController 파라미터 수정

--- src/main/java/com/ssafy/tripmate/place/controller/PlaceController.java
+++ src/main/java/com/ssafy/tripmate/place/controller/PlaceController.java
@@ -3,16 +3,14 @@ package com.ssafy.tripmate.place.controller;
 import com.ssafy.tripmate.place.dto.PlaceResponseDto;
 import com.ssafy.tripmate.place.dto.SearchFilter;
 import com.ssafy.tripmate.place.service.PlaceService;
-import com.ssafy.tripmate.place.service.PlaceServiceImpl;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.boot.model.process.internal.UserTypeResolution;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,7 +32,7 @@ public class PlaceController {
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "장소 조회 성공"),
             @ApiResponse(responseCode = "204", description = "장소 정보 없음"),
             @ApiResponse(responseCode = "500", description = "서버 에러")})
-    public ResponseEntity<?> findPlaces(@RequestBody SearchFilter searchFilter) {
+    public ResponseEntity<?> findPlaces(@ModelAttribute SearchFilter searchFilter) {
         List<PlaceResponseDto> places = service.findAll(searchFilter);
         if (places.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);

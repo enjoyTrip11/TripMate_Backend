c42309f71e525f7054e4bc7846669d17e19baab7
yeondori <yeonseo.jung0@gmail.com>
2024-05-21 14:38:58 +0900

    feat: PlaceDto

---
+++ src/main/java/com/ssafy/tripmate/place/dto/PlaceResponseDto.java
@@ -0,0 +1,34 @@
+package com.ssafy.tripmate.place.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PlaceResponseDto {
+    private Integer locationId;
+    private String title;
+    private String addr1;
+    private String addr2;
+    private String zipcode;
+    private String firstImage;
+    private String secondImage;
+    private Integer sidoCode;
+    private Double latitude;
+    private Double longitude;
+    private String overview;
+    private Integer contentTypeId;
+
+    public PlaceResponseDto(Place place) {
+        this.locationId = place.getLocationId();
+        this.title = place.getTitle();
+        this.addr1 = place.getAddr1();
+        this.addr2 = place.getAddr2();
+        this.zipcode = place.getZipcode();
+        this.firstImage = place.getFirstImage();
+        this.secondImage = place.getSecondImage();
+        this.sidoCode = place.getSidoCode();
+        this.latitude = place.getLatitude();
+        this.longitude = place.getLongitude();
+        this.overview = place.getOverview();
+        this.contentTypeId = place.getContentTypeId();
+    }
+}

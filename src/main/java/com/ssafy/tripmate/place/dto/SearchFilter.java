dfec3d57a1a86fd538504bf9d0d06e09769e43d1
yeondori <yeonseo.jung0@gmail.com>
2024-05-21 15:16:03 +0900

    feat: PlaceDto(SearchFilter, Exception)

--- src/main/java/com/ssafy/tripmate/place/dto/SearchFilter.java
+++ src/main/java/com/ssafy/tripmate/place/dto/SearchFilter.java
@@ -11,4 +11,8 @@ public class SearchFilter {
     private String keyword;
     private Integer sidoCode;
     private Integer contentTypeId;
+
+    // 정렬을 위한 위치 정보
+    private Double latitude;
+    private Double longitude;
 }

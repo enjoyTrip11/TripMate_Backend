42a110b6689bca225fa0e441c4444a5a4d564b5c
yeondori <yeonseo.jung0@gmail.com>
2024-05-21 17:49:22 +0900

    feat: HotPlaceDto

--- src/main/java/com/ssafy/tripmate/place/dao/HotPlaceDao.java
+++ src/main/java/com/ssafy/tripmate/place/dao/HotPlaceDao.java
@@ -1,13 +1,19 @@
 package com.ssafy.tripmate.place.dao;
 
+import com.ssafy.tripmate.place.dto.HotPlace;
+import com.ssafy.tripmate.place.dto.HotPlaceResponseDto;
 import com.ssafy.tripmate.place.dto.Place;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
 import java.util.List;
 
+@Mapper
 public interface HotPlaceDao {
 
-    int insert(int boardId, int userId) throws SQLException;
+    HotPlace searchByBoardUser(int boardId, int userId) throws SQLException;
+    HotPlace searchById(int hotplaceId) throws SQLException;
+    void insert(int boardId, int userId) throws SQLException;
 
     void delete(int hotplaceId) throws SQLException;
 
@@ -15,5 +21,5 @@ public interface HotPlaceDao {
 
     List<Place> searchHotPlaceByUser(int userId) throws SQLException;
 
-    List<Place> searchAllHotPlace() throws SQLException;
+    List<HotPlaceResponseDto> searchAllHotPlace() throws SQLException;
 }
